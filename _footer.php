0960aefc1aaee1e2b00dc72c5384653bd0464de2
Fellype S <fellypesiqueira8@gmail.com>
2024-03-14 16:42:05 -0300

    css changes

--- _footer.php
+++ _footer.php
@@ -1,23 +1,26 @@
 </main>
-        <footer>
-            <a href="index.php"><i class="fa-solid fa-house-chimney"></i></a>
-            <div>
-                <div>&copy; 2024 Eu mesmo.</div>
-                <a href="privacy.php">Políticas de Privacidade</a>
-            </div>
-            <a href="#wrap"><i class="fa-solid fa-chevron-up"></i></a>
-        </footer>
+</div>
+<footer>
+    <a href="index.php"><i class="fa-solid fa-house-chimney"></i></a>
+    <div>
+        <div>&copy; 2024 Eu mesmo.</div>
+        <a href="privacy.php">Políticas de Privacidade</a>
     </div>
+    <a href="#wrap"><i class="fa-solid fa-chevron-up"></i></a>
+</footer>
+</div>
 
 
-    <?php // Import the javascrispt ?>
-    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-auth-compat.js"></script>
+<?php // Import the javascrispt 
+?>
+<script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-auth-compat.js"></script>
 
 
-    <?php // Import the javascrispt ?>
-    <script src="assets/js/global.js"></script>
-    <script src="assets/js/<?php echo $page["js"] ?> "></script>
+<?php // Import the javascrispt 
+?>
+<script src="assets/js/global.js"></script>
+<script src="assets/js/<?php echo $page["js"] ?> "></script>
 
 </body>
 
--- _header.php
+++ _header.php
@@ -11,46 +11,48 @@
 </head>
 
 <body>
-    <div id="wrap">
-        <header>
+    <header>
+        <div class="header-nav-container">
             <div class="header-title-logo">
                 <a href="index.php" title="Página inicial">
-                    <img class="logo" src="assets/img/<?php echo $site["logo"]?>" alt="Logotipo de Hello World">
+                    <img class="logo" src="assets/img/<?php echo $site["logo"] ?>" alt="Logotipo de Hello World">
                 </a>
-                <div class="header-title">
-                    <h1>Hello World</h1>
-                    <small>Lendo e Aprendendo.</small>
-                </div>
+            </div>
+            <div class="header-title">
+                <h1>Hello World</h1>
+                <small>Lendo e Aprendendo.</small>
             </div>
             <div class="header-search">
                 <form class="form-wrapper" action="" method="get">
-                    <input type="search" name="q" id="search" placeholder="Pesquisar...">
-                    <button type="submit" class="search-icon"><i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
+                <input class="input-search" name="q" id="search" placeholder="Pesquisar...">   
+                <button type="submit" class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </form>
             </div>
-        </header>
-        <nav>
-            <a href="index.php" title="Página Inicial">
-                <i class="fa-solid fa-house-chimney" style="text-decoration: none;"></i>
+        </div>
+    </header>
+    <nav>
+        <a href="index.php" title="Página Inicial">
+            <i class="fa-solid fa-house-chimney" style="text-decoration: none;"></i>
 
-                <span>Início</span>
-            </a>
+            <span>Início</span>
+        </a>
 
-            <a href="contacts.php" title="Fale Conosco">
-                <i class="fa-solid fa-comment-dots"></i>
-                <span>Contatos</span>
-            </a>
+        <a href="contacts.php" title="Fale Conosco">
+            <i class="fa-solid fa-comment-dots"></i>
+            <span>Contatos</span>
+        </a>
 
-            <a href="about.php" title="Sobre nós">
-                <i class="fa-solid fa-circle-info"></i>
-                <span>Sobre</span>
-            </a>
-            <a id="userAccess" href="login.php" title="Logue-se">
-            <img id="userImg"src="" alt="">
+        <a href="about.php" title="Sobre nós">
+            <i class="fa-solid fa-circle-info"></i>
+            <span>Sobre</span>
+        </a>
+        <a id="userAccess" href="login.php" title="Logue-se">
+            <img id="userImg" src="" alt="">
             <i id="userIcon" class="fa-solid fa-right-to-bracket"></i>
-                <span id="userLabel">Login</span>
-            </a>
-        </nav>
+            <span id="userLabel">Login</span>
+        </a>
+    </nav>
 
-        <main>
+    <div id="container">
+        <div id="wrap">
+            <main>
